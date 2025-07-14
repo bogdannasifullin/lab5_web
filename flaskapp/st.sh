e5e099d7da42d5fce548eba78f612edd28206474
bogdannasifullin <bnasifullin@gmail.com>
2025-07-14 19:58:46 +0300

    Update and rename .gitkeep to st.sh

---
+++ flaskapp/st.sh
@@ -0,0 +1,11 @@
+
+#!/bin/bash
+
+gunicorn --bind 127.0.0.1:5000 wsgi:app & APP_PID=$!
+sleep 5
+echo "start client"
+python3 client.py
+sleep 5
+echo "Stopping server with PID $APP_PID"
+kill -TERM $APP_PID
+exit 0
